8fa1ec64c91a22564e2f16f3f29be5cd4d2b4dea
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-03-08 20:45:23 +0600

    first commit

---
+++ bin/variable_2.dart
@@ -0,0 +1,22 @@
+ void main(){
+  var dynamicVar=42;
+  int fixedvar=100;
+  double pi= 314;
+  String msg= "hello shoab";
+  bool isfun= true;
+
+
+  print(dynamicVar);
+  print(fixedvar);
+  print(pi);
+  print(msg);
+  print(isfun);
+
+  String number="42";
+  int parsenumber=int.parse(number);
+  print(parsenumber);
+
+  double price= 99.99;
+  print(price.round());
+  print(parsenumber.toString());
+ }
