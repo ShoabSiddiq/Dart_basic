99015a3b843206c39ab7a3a3457620608c7bdae5
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-03-11 21:02:19 +0600

    first commit

---
+++ bin/operator.dart
@@ -0,0 +1,30 @@
+void main(){
+  int a=5,b=10;  //arithmetic operator
+
+  print("Addition ${a+b}");
+  print("Sub ${a-b}");
+  print("Multiplication ${a*b}");
+  print("Division ${a/b}");
+  print("Reminder ${a%b}");
+
+// relational operator
+
+  print("a is equal to b :${a==b}");
+  print("a is not equal to b: ${a!=b}");
+  print("a is greater than  b: ${a>b}");
+  print("a is less b: ${a<b}");
+
+  // logical operator
+
+  bool x= true,y=false;
+
+  print("x and y ${x && y}");
+  print("x or y ${x || y}");
+
+
+  int v=10;
+  int  c=-v;
+
+  v++;
+  c--;
+}
