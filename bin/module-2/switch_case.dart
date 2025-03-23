f8ab2f061bbfe15fec997b43f3b09027adc4425b
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-03-23 21:09:41 +0600

    first commit

---
+++ bin/module-2/switch_case.dart
@@ -0,0 +1,46 @@
+import 'dart:io'; //user theke input
+
+main() {
+  print('Enter day: ');
+  String? day = stdin.readLineSync(); //user theke input not nullable
+
+  switch (day) {
+    case 'Fri':
+      print('Relax');
+      break;
+    case 'Sat':
+      print('Outgoing');
+      break;
+    case 'Sun':
+      print('Gym');
+      break;
+  }
+  // multiple case
+
+  int month = 7;
+
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      print('winter');
+      break;
+    case 4:
+    case 5:
+    case 6:
+      print('spring');
+      break;
+    case 7:
+    case 8:
+    case 9:
+      print('summer');
+      break;
+    case 10:
+    case 11:
+      print('autumn');
+      break;
+
+    default:
+      print('invalid month');
+  }
+}
