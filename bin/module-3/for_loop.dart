fa73108803c3ff5b40eb854d789ee09cec51b48f
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-03-27 15:43:39 +0600

    first commit

--- bin/module-3/for_loop.dart
+++ bin/module-3/for_loop.dart
@@ -6,4 +6,12 @@ main(){
   for(int i=1; i<=10; i++){
     print('15 * $i = ${15*i}');
   }
+  print('\n');
+
+  List<String>students= ['karim','rafiq','kamal','sabbir','hessen'];
+  // print('Student index 2 : ${students[2]}');
+  for(int index=0; index<students.length; index++){
+    print('Student $index name : ${students[index]}');
+  }
+
 }
