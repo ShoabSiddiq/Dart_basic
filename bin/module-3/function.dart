0b556360b49a0449e62ad4498c403fe4c22f3194
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-03-29 19:57:10 +0600

    loop

---
+++ bin/module-3/function.dart
@@ -0,0 +1,15 @@
+main(){
+   //without function
+
+  double length1=0.5;
+  double width1=0.5;
+  double area1=length1*width1;
+  print('Area1 = $area1');
+
+  double length2=0.75;
+  double width2=12.65;
+  double area2=length2*width2;
+  print('Area2 = $area2');
+
+}
+//with function
