301c3047f7ec067ab9918f438b0110941694614f
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-04-09 20:46:47 +0600

    init

---
+++ bin/oop_concept/constructor.dart
@@ -0,0 +1,16 @@
+main(){
+  Car honda=Car('Honda', 'Civic', 2024);
+  print(honda.brand);
+  print(honda.model);
+  print(honda.year);
+}
+
+
+class Car{
+  String brand;
+  String model;
+  int year;
+
+  // Constructor
+  Car(this.brand,this.model,this.year); //this means car class k refer korse
+}
