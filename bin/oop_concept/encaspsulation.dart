0597799485d5f1467119b66e9c7adbd36eac06d3
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-04-12 21:42:55 +0600

    init

--- bin/oop_concept/encaspsulation.dart
+++ bin/oop_concept/encaspsulation.dart
@@ -1,6 +1,11 @@
 import 'resturant.dart';
 
 main(){
-  Restaurant bfc = Restaurant();
+  Restaurant bfc = Restaurant('kfc');
+  bfc.name;
+  print(bfc.location);
+  print(bfc.restaurentId);   //private value access
+  bfc.setId=3758;
+  print(bfc.restaurentId); // id updated used by setter method
   bfc.order('bbq chicken');
 }
--- bin/oop_concept/resturant.dart
+++ bin/oop_concept/resturant.dart
@@ -1,4 +1,9 @@
 class Restaurant{
+  String name;
+  String location='Dhaka';
+  int _id= 2023;
+  Restaurant(this.name);
+
 
   order(String item){
     print('$item ordered');
@@ -13,4 +18,10 @@ class Restaurant{
   _shopping(String item){         //under score dile private clsaa hoye jay
     print('$item shopping');
   }
+  //getter method
+  int get restaurentId=> _id; /// private value access korte use kora hoy
+/// setter method
+set setId(int value){
+  _id=value;
+}
 }
