18b37d27409c5dfa6b414217858c8894f34fe134
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-04-13 10:56:13 +0600

    init

--- bin/oop_concept/encaspsulation.dart
+++ bin/oop_concept/encaspsulation.dart
@@ -5,7 +5,7 @@ main(){
   bfc.name;
   print(bfc.location);
   print(bfc.restaurentId);   //private value access
-  bfc.setId=3758;
+  bfc.setId = 3758;
   print(bfc.restaurentId); // id updated used by setter method
   bfc.order('bbq chicken');
 }
