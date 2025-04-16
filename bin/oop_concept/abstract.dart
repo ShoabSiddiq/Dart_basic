373d7ea3125329bd333ffab763f290311f56bc28
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-04-16 21:35:33 +0600

    init

---
+++ bin/oop_concept/abstract.dart
@@ -0,0 +1,18 @@
+class Student{
+  void attendClass(){     //void use krleo hbe na krleo hbe
+   print('joined class');
+  }
+  void examSubmit(){
+
+  }
+}
+
+class person extends Student{
+  String name;
+  person(this.name);
+}
+
+main(){
+  person rafi = person('Rafi');
+  rafi.attendClass();
+}
