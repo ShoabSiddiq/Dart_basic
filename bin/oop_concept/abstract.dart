5ea6e7e4d7d9fdd3213c023368c250b1cdcb0ff6
ShoabSiddiq <shoabsiddiq8@gmail.com>
2025-04-17 21:37:38 +0600

    init

--- bin/oop_concept/abstract.dart
+++ bin/oop_concept/abstract.dart
@@ -2,15 +2,15 @@
 abstract class Student{  // directly access kora jabe na, inherit r maddhome krte hobe
                           // but object create r maddhome kora possible na
 
-   attendClass();           //abstract method
+   attendClass();           //abstract method,must be implemented in child class
    examSubmit(){
-
+      print('hello dart');
   }
 }
 
 class person extends Student{
   String name;
-  person(this.name);
+  person(this.name);  // Constructor to initialize name
 
   @override
   attendClass() {          //abstract method k must override diye call korte hobe
@@ -18,8 +18,27 @@ class person extends Student{
   }
 }
 
+class BusinessMan implements Student{ //implements use korle joto type r method thakbe sob gula override korte hobe
+  String name;
+  BusinessMan(this.name);  // Constructor to initialize name
+
+  @override
+  attendClass() {          //implement  method k must override diye call korte hobe
+    print('$name joined class');
+  }
+
+  @override
+  examSubmit() {
+    print('$name BusinessMan joined class');
+  }
+}
+
 main(){
   person rafi = person('Rafi');
   rafi.attendClass();
+  rafi.examSubmit();
+
+  BusinessMan Rahim= BusinessMan('Rahim');
+  Rahim.examSubmit();
 }
 
